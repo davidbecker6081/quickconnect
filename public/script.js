0c1b6b7c34f0b9833351d342b569fa268f64dcc3
David Becker <david@beckersite.com>
2019-11-18 13:19:12 -0700

    Add endpoints functionality

--- public/script.js
+++ public/script.js
@@ -1,13 +1,86 @@
 const audio = document.querySelector('audio');
 
+const healthCheck = document.querySelector('.health-check');
+
+const getAllMessages = document.querySelector('.get-all-messages');
+const getAllMessagesLimit = document.querySelector('.get-all-messages-limit');
+
+const getMessages = document.querySelector('.get-messages');
+const getMessagesLimit = document.querySelector('.get-messages-limit');
+
+const postMessage = document.querySelector('.post-message');
+
+audio.currentTime = 3;
 audio.addEventListener('ended', () => {
-  console.log('src:', audio.src)
 	if (audio.src === '/assets/Watermelon Man-[AudioTrimmer.com].mp3') {
-    console.log('watermelon to james brown')
 		audio.src = '/assets/james-brown-instrumental.mp3';
 	} else {
-    console.log('switch')
 		audio.src = '/assets/Watermelon Man-[AudioTrimmer.com].mp3';
 	}
 	audio.play();
+});
+
+healthCheck.addEventListener('click', () => {
+	const win = window.open('/public/v1/health-check');
+	win.focus();
+});
+
+getAllMessages.addEventListener('click', () => {
+	const win = window.open('/public/v1/messages/12345/54321');
+	win.focus();
+});
+
+getAllMessagesLimit.addEventListener('click', () => {
+	const win = window.open('/public/v1/messages/12345/54321/?limit=true');
+	win.focus();
+});
+
+getMessages.addEventListener('click', () => {
+	const win = window.open('/public/v1/messages/12345');
+	win.focus();
+});
+
+getMessagesLimit.addEventListener('click', () => {
+	const win = window.open('/public/v1/messages/12345/?limit=true');
+	win.focus();
+});
+
+postMessage.addEventListener('click', () => {
+	const message = {
+		message: 'This is a message!',
+		sender_id: '12345',
+		recipient_id: '54321'
+	};
+	fetch('/public/v1/message', {
+		method: 'POST',
+		headers: { 'Content-type': 'application/json' },
+		body: JSON.stringify(message)
+	}).then(res => {
+		return res.json();
+	}).then(res => {
+		console.log(res);
+
+		const jsonResponse = document.createElement('textarea');
+
+		jsonResponse.className = 'post-response';
+		const textNode = document.createTextNode(`${JSON.stringify(res, null, 4)}`);
+		jsonResponse.appendChild(textNode);
+
+		const closeBtn = document.createElement('button');
+		closeBtn.className = 'close';
+		const btnText = document.createTextNode('X');
+		closeBtn.appendChild(btnText);
+		document.body.appendChild(closeBtn);
+		jsonResponse.readonly = true;
+
+		document.body.appendChild(jsonResponse);
+
+		const close = document.querySelector('.close');
+		const postResponse = document.querySelector('.post-response');
+		close.addEventListener('click', () => {
+			document.body.removeChild(postResponse);
+			document.body.removeChild(close);
+		});
+
+	});
 });
